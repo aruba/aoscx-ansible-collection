031610145315b8b9da0b4121d51abcbbc419a451
Isela Paola Aley De la Hoz <isela-aley.de-la-hoz@hpe.com>
2023-01-01 05:21:52 -0800

    Fix DNS Ansible Module
    DNS setup and delete functions need session argument, but it is not
    given.
    This change adds session argument to these functions. Also, idempotency
    for delete state is fixed.
    
    (cherry picked from commit 5ae9f98c3c509e2ba10f510aec94f7303708d16f)

--- docs/aoscx_dns.md
+++ docs/aoscx_dns.md
@@ -1,108 +1,243 @@
-# module: aoscx_dns
+# Domain Name System
 
-description: This modules provides configuration management of DNS on AOS-CX
-devices.
+Domain Name System (DNS) module for Ansible
 
-##### ARGUMENTS
+Version added: 4.1.0
 
-```YAML
-  mgmt_nameservers:
-    description: >
-      Primary and secondary nameservers on mgmt interface. The key of the
-      dictionary is primary or secondary and value is the respective IP
-      address.
-    type: dict
-  dns_domain_name:
-    description: >
-      Domain name used for name resolution by the DNS client, if
-      'dns_domain_list' is not configured.
-    type: str
-  dns_domain_list:
-    description: >
-      Domain list names to be used for address resolution, keyed by the
-      resolution priority order.
-    type: dict
-  dns_name_servers:
-    description: >
-      Name servers to be used for address resolution, keyed by the resolution
-      priority order.
-    type: dict
-  vrf:
-    description: VRF name where DNS configuration is added.
-    type: str
-    required: false
-  dns_host_v4_address_mapping:
-    description: >
-      List of static host address configurations and the IPv4 address
-      associated with them.
-    type: dict
-  state:
-    description: Create or Update or Delete DNS configuration on the switch.
-    default: create
-    choices:
-      - create
-      - update
-      - delete
-    required: false
-    type: str
+ - [Synopsis](#synopsis)
+ - [Parameters](#parameters)
+ - [Examples](#examples)
+
+## Synopsis
+
+This module allows configuration management on DNS for AOSCX devices.
+
+## Parameters
+
+| Parameter                 | Type | Choices/Defaults                          | Required | Comments                                                                                                                                       |
+|:--------------------------|:-----|:------------------------------------------|:--------:|:-----------------------------------------------------------------------------------------------------------------------------------------------|
+| `mgmt_nameservers`        | dict |                                           | [ ]      | Primary and secondary nameservers on mgmt interface. The key of the dictionary is primary or secondary and value is the respective IP address. |
+| `domain_name`             | str  |                                           | [ ]      | Domain name used for name resolution by the DNS client, if `domain_list` is not configured.                                                    |
+| `domain_list`             | dict |                                           | [ ]      | Domain list names to be used for address resolution, keyed by the resolution priority order.                                                   |
+| `name_servers`            | dict |                                           | [ ]      | Name servers to be used for address resolution, keyed by the resolution priority order.                                                        |
+| `vrf`                     | str  |                                           | [x]      | VRF name where DNS configuration is added.                                                                                                     |
+| `host_v4_address_mapping` | dict |                                           | [ ]      | List of static host address configurations and the IPv4 address associated with them.                                                          |
+| `host_v6_address_mapping` | dict |                                           | [ ]      | List of static host address configurations and the IPv6 address associated with them.                                                          |
+| `state`                   | str  | [`create`, `update`, `delete`] / `create` | [ ]      | Create, update, or delete a DNS.                                                                                                               |
+
+## Examples
+
+### Create DNS
+
+Before Device Configuration
+```
+vrf green
+ntp server pool.ntp.org minpoll 4 maxpoll 4 iburst
+ntp enable
+!
+!
+!
+!
+!
+!
+ssh server vrf mgmt
+vlan 1
+interface mgmt
+    no shutdown
+    ip static 172.25.0.2/24
 ```
 
-##### EXAMPLES
-
+Playbook:
 ```YAML
 - name: DNS configuration creation
   aoscx_dns:
     mgmt_nameservers:
      "Primary": "10.10.2.10"
      "Secondary": "10.10.2.10"
-    dns_domain_name: "hpe.com"
-    dns_domain_list:
+    domain_name: "hpe.com"
+    domain_list:
       0: "hp.com"
       1: "aru.com"
       2: "sea.com"
-    dns_name_servers:
+    name_servers:
       0: "4.4.4.8"
       1: "4.4.4.10"
-    dns_host_v4_address_mapping:
+    host_v4_address_mapping:
       "host1": "5.5.44.5"
       "host2": "2.2.44.2"
     vrf: "green"
+```
 
+After Device Configuration:
+```
+vrf green
+ntp server pool.ntp.org minpoll 4 maxpoll 4 iburst
+ntp enable
+!
+!
+!
+!
+!
+!
+ssh server vrf mgmt
+vlan 1
+interface mgmt
+    no shutdown
+    ip static 172.25.0.2/24
+    nameserver 10.10.2.10
+ip dns domain-name hpe.com vrf green
+ip dns domain-list hp.com vrf green
+ip dns domain-list aru.com vrf green
+ip dns domain-list sea.com vrf green
+ip dns server-address 4.4.4.8 vrf green
+ip dns server-address 4.4.4.10 vrf green
+ip dns host host1 5.5.44.5 vrf green
+ip dns host host2 2.2.44.2 vrf green
+```
+
+### Update DNS
+
+Before Device Configuration:
+```
+vrf green
+ntp server pool.ntp.org minpoll 4 maxpoll 4 iburst
+ntp enable
+!
+!
+!
+!
+!
+!
+ssh server vrf mgmt
+vlan 1
+interface mgmt
+    no shutdown
+    ip static 172.25.0.2/24
+    nameserver 10.10.2.10
+ip dns domain-name hpe.com vrf green
+ip dns domain-list hp.com vrf green
+ip dns domain-list aru.com vrf green
+ip dns domain-list sea.com vrf green
+ip dns server-address 4.4.4.8 vrf green
+ip dns server-address 4.4.4.10 vrf green
+ip dns host host1 5.5.44.5 vrf green
+ip dns host host2 2.2.44.2 vrf green
+```
+
+Playbook:
+```YAML
 - name: DNS configuration update
   aoscx_dns:
     mgmt_nameservers:
       "Primary": "10.10.2.15"
       "Secondary": "10.10.2.25"
-    dns_domain_name: "hpe.com"
-    dns_domain_list:
+    domain_name: "hpe.com"
+    domain_list:
       0: "hpe.com"
       1: "aruba.com"
       2: "seach.com"
-    dns_name_servers:
+    name_servers:
       0: "4.4.4.10"
       1: "4.4.4.12"
-    dns_host_v4_address_mapping:
+    host_v4_address_mapping:
       "host1": "5.5.5.5"
       "host2": "2.2.45.2"
     vrf: "green"
     state: update
+```
 
+After Device Configuration:
+```
+vrf green
+ntp server pool.ntp.org minpoll 4 maxpoll 4 iburst
+ntp enable
+!
+!
+!
+!
+!
+!
+ssh server vrf mgmt
+vlan 1
+interface mgmt
+    no shutdown
+    ip static 172.25.0.2/24
+    nameserver 10.10.2.15
+ip dns domain-name hpe.com vrf green
+ip dns domain-list hpe.com vrf green
+ip dns domain-list aruba.com vrf green
+ip dns domain-list seach.com vrf green
+ip dns server-address 4.4.4.10 vrf green
+ip dns server-address 4.4.4.12 vrf green
+ip dns host host1 5.5.5.5 vrf green
+ip dns host host2 2.2.45.2 vrf green
+```
+
+### Delete DNS
+
+Before Device Configuration:
+```
+vrf green
+ntp server pool.ntp.org minpoll 4 maxpoll 4 iburst
+ntp enable
+!
+!
+!
+!
+!
+!
+ssh server vrf mgmt
+vlan 1
+interface mgmt
+    no shutdown
+    ip static 172.25.0.2/24
+    nameserver 10.10.2.15
+ip dns domain-name hpe.com vrf green
+ip dns domain-list hpe.com vrf green
+ip dns domain-list aruba.com vrf green
+ip dns domain-list seach.com vrf green
+ip dns server-address 4.4.4.10 vrf green
+ip dns server-address 4.4.4.12 vrf green
+ip dns host host1 5.5.5.5 vrf green
+ip dns host host2 2.2.45.2 vrf green
+```
+
+Playbook:
+```YAML
 - name: DNS configuration deletion
   aoscx_dns:
     mgmt_nameservers:
       "Primary": "10.10.2.15"
       "Secondary": "10.10.2.25"
-    dns_domain_name: "hp.com"
-    dns_domain_list:
+    domain_name: "hp.com"
+    domain_list:
       0: "hpe.com"
       1: "aruba.com"
       2: "seach.com"
-    dns_name_servers:
+    name_servers:
       0: "4.4.4.10"
       1: "4.4.4.12"
-    dns_host_v4_address_mapping:
+    host_v4_address_mapping:
       "host1": "5.5.5.5"
       "host2": "2.2.45.2"
     vrf: "green"
     state: delete
+```
+
+After Device Configuration:
+```
+vrf green
+ntp server pool.ntp.org minpoll 4 maxpoll 4 iburst
+ntp enable
+!
+!
+!
+!
+!
+!
+ssh server vrf mgmt
+vlan 1
+interface mgmt
+    no shutdown
+    ip static 172.25.0.2/24
 ```
--- plugins/modules/aoscx_dns.py
+++ plugins/modules/aoscx_dns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (C) Copyright 2019-2022 Hewlett Packard Enterprise Development LP.
+# (C) Copyright 2019-2023 Hewlett Packard Enterprise Development LP.
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
@@ -30,30 +30,41 @@ options:
       dictionary is primary or secondary and value is the respective IP
       address.
     type: dict
-  dns_domain_name:
+    required: false
+  domain_name:
     description: >
       Domain name used for name resolution by the DNS client, if
-      'dns_domain_list' is not configured.
+      'domain_list' is not configured.
     type: str
-  dns_domain_list:
+    required: false
+  domain_list:
     description: >
       Domain list names to be used for address resolution, keyed by the
       resolution priority order.
     type: dict
-  dns_name_servers:
+    required: false
+  name_servers:
     description: >
       Name servers to be used for address resolution, keyed by the resolution
       priority order.
     type: dict
+    required: false
   vrf:
     description: VRF name where DNS configuration is added.
     type: str
-    required: false
-  dns_host_v4_address_mapping:
+    required: true
+  host_v4_address_mapping:
     description: >
       List of static host address configurations and the IPv4 address
       associated with them.
     type: dict
+    required: false
+  host_v6_address_mapping:
+    description: >
+      List of static host address configurations and the IPv6 address
+      associated with them.
+    type: dict
+    required: false
   state:
     description: Create or Update or Delete DNS configuration on the switch.
     default: create
@@ -61,8 +72,8 @@ options:
       - create
       - update
       - delete
-    required: false
     type: str
+    required: false
 """
 
 EXAMPLES = """
@@ -72,15 +83,15 @@ EXAMPLES = """
     mgmt_nameservers:
       "Primary": "10.10.2.10"
       "Secondary": "10.10.2.10"
-    dns_domain_name: "hpe.com"
-    dns_domain_list:
+    domain_name: "hpe.com"
+    domain_list:
       0: "hp.com"
       1: "aru.com"
       2: "sea.com"
-    dns_name_servers:
+    name_servers:
       0: "4.4.4.8"
       1: "4.4.4.10"
-    dns_host_v4_address_mapping:
+    host_v4_address_mapping:
       "host1": "5.5.44.5"
       "host2": "2.2.44.2"
     vrf: "green"
@@ -90,15 +101,15 @@ EXAMPLES = """
     mgmt_nameservers:
       "Primary": "10.10.2.15"
       "Secondary": "10.10.2.25"
-    dns_domain_name: "hpe.com"
-    dns_domain_list:
+    domain_name: "hpe.com"
+    domain_list:
       0: "hpe.com"
       1: "aruba.com"
       2: "seach.com"
-    dns_name_servers:
+    name_servers:
       0: "4.4.4.10"
       1: "4.4.4.12"
-    dns_host_v4_address_mapping:
+    host_v4_address_mapping:
       "host1": "5.5.5.5"
       "host2": "2.2.45.2"
     vrf: "green"
@@ -109,15 +120,15 @@ EXAMPLES = """
     mgmt_nameservers:
       "Primary": "10.10.2.15"
       "Secondary": "10.10.2.25"
-    dns_domain_name: "hp.com"
-    dns_domain_list:
+    domain_name: "hp.com"
+    domain_list:
       0: "hpe.com"
       1: "aruba.com"
       2: "seach.com"
-    dns_name_servers:
+    name_servers:
       0: "4.4.4.10"
       1: "4.4.4.12"
-    dns_host_v4_address_mapping:
+    host_v4_address_mapping:
       "host1": "5.5.5.5"
       "host2": "2.2.45.2"
     vrf: "green"
@@ -126,109 +137,132 @@ EXAMPLES = """
 
 RETURN = r""" # """
 
+
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.arubanetworks.aoscx.plugins.module_utils.aoscx_pyaoscx import (  # NOQA
-    get_pyaoscx_session,
-)
+
+try:
+    from pyaoscx.dns import Dns
+    from pyaoscx.vrf import Vrf
+
+    HAS_PYAOSCX = True
+except ImportError:
+    HAS_PYAOSCX = False
+
+if HAS_PYAOSCX:
+    from ansible_collections.arubanetworks.aoscx.plugins.module_utils.aoscx_pyaoscx import (  # NOQA
+        get_pyaoscx_session,
+    )
+
+
+def get_argument_spec():
+    argument_spec = {
+        "mgmt_nameservers": {
+            "type": "dict",
+            "required": False,
+        },
+        "domain_name": {
+            "type": "str",
+            "required": False,
+        },
+        "domain_list": {
+            "type": "dict",
+            "required": False,
+        },
+        "name_servers": {
+            "type": "dict",
+            "required": False,
+        },
+        "vrf": {
+            "type": "str",
+            "required": True,
+        },
+        "host_v4_address_mapping": {
+            "type": "dict",
+            "required": False,
+        },
+        "host_v6_address_mapping": {
+            "type": "dict",
+            "required": False,
+        },
+        "state": {
+            "type": "str",
+            "required": False,
+            "default": "create",
+            "choices": ["create", "update", "delete"],
+        },
+    }
+
+    return argument_spec
 
 
 def main():
-    """
-    Ansible module to configure DNS on AOS-CX switch
-    """
-    module_args = dict(
-        mgmt_nameservers=dict(type="dict", required=False),
-        dns_domain_list=dict(type="dict", required=False),
-        dns_domain_name=dict(type="str", required=False),
-        dns_name_servers=dict(type="dict", required=False),
-        vrf=dict(type="str", required=False),
-        dns_host_v4_address_mapping=dict(type="dict", required=False),
-        state=dict(
-            type="str",
-            default="create",
-            choices=["create", "delete", "update"],
-        ),
-    )
-    # ArubaModule
     ansible_module = AnsibleModule(
-        argument_spec=module_args, supports_check_mode=True
+        argument_spec=get_argument_spec(), supports_check_mode=True
     )
 
-    # Set Variables
-    mgmt_nameservers = ansible_module.params["mgmt_nameservers"]
-    dns_domain_name = ansible_module.params["dns_domain_name"]
-    dns_domain_list = ansible_module.params["dns_domain_list"]
-    vrf_name = ansible_module.params["vrf"]
-    dns_name_servers = ansible_module.params["dns_name_servers"]
-    dns_host_v4_address_mapping = ansible_module.params[
-        "dns_host_v4_address_mapping"
-    ]
-    state = ansible_module.params["state"]
+    if not HAS_PYAOSCX:
+        ansible_module.fail_json(
+            msg="Could not find the PYAOSCX SDK. Make sure it is installed."
+        )
 
-    result = dict(changed=False)
+    result = {"changed": False}
 
     if ansible_module.check_mode:
         ansible_module.exit_json(**result)
 
-    try:
-        from pyaoscx.device import Device
-    except Exception as e:
-        ansible_module.fail_json(msg=str(e))
+    # Set Variables
+    mgmt_nameservers = ansible_module.params["mgmt_nameservers"]
+    domain_name = ansible_module.params["domain_name"]
+    domain_list = ansible_module.params["domain_list"]
+    vrf_name = ansible_module.params["vrf"]
+    name_servers = ansible_module.params["name_servers"]
+    host_v4_address_mapping = ansible_module.params["host_v4_address_mapping"]
+    host_v6_address_mapping = ansible_module.params["host_v6_address_mapping"]
+    state = ansible_module.params["state"]
+
+    session = get_pyaoscx_session(ansible_module)
+
+    vrf = Vrf(session, vrf_name)
 
     try:
-        session = get_pyaoscx_session(ansible_module)
-    except Exception as e:
-        ansible_module.fail_json(
-            msg="Could not get PYAOSCX Session: {0}".format(str(e))
-        )
+        vrf.get()
+    except Exception:
+        ansible_module.fail_json(msg="VRF {0} doesn't exist.".format(vrf_name))
 
-    device = Device(session)
+    dns = Dns(session, vrf_name)
+
+    modified_op = False
+    modified_op2 = False
 
     if state == "delete":
-        # Modifed Variables
-        modified_op = False
-        modified_op2 = False
+        if mgmt_nameservers != {}:
+            modified_op = dns.delete_mgmt_nameservers(session)
 
-        # Create DNS object
-        dns = device.dns(vrf=vrf_name)
-
-        # Delete MGMT nameservers
-        if mgmt_nameservers is not None:
-            # Delete it
-            modified_op = dns.delete_mgmt_nameservers()
-
-        # Delete DNS
         dns.delete_dns(
-            dns_domain_name,
-            dns_domain_list,
-            dns_name_servers,
-            dns_host_v4_address_mapping,
+            domain_name,
+            domain_list,
+            name_servers,
+            host_v4_address_mapping,
+            host_v6_address_mapping,
         )
-        # Check if dns was modified
         modified_op2 = dns.was_modified()
-
-        # Changed
         result["changed"] = modified_op or modified_op2
 
-    if state == "create" or state == "update":
-        # Modifed Variables
-        modified_op = False
-        modified_op2 = False
-
-        # Create DNS object
-        dns = device.dns(
-            vrf=vrf_name,
-            domain_name=dns_domain_name,
-            domain_list=dns_domain_list,
-            domain_servers=dns_name_servers,
-            host_v4_address_mapping=dns_host_v4_address_mapping,
+    else:
+        dns = Dns(
+            session,
+            vrf_name=vrf_name,
+            domain_name=domain_name,
+            domain_list=domain_list,
+            domain_servers=name_servers,
+            host_v4_address_mapping=host_v4_address_mapping,
+            host_v6_address_mapping=host_v6_address_mapping,
         )
 
-        # Check if dns was modified
         modified_op = dns.was_modified()
 
         # Set MGMT name servers
-        if mgmt_nameservers is not None:
+        if mgmt_nameservers != {}:
             primary = None
             secondary = None
             # Get Primary and Secondary
@@ -237,15 +271,15 @@ def main():
                     primary = value
                 elif key.lower() == "secondary":
                     secondary = value
-            # Set up
             modified_op2 = dns.setup_mgmt_nameservers(
-                primary=primary, secondary=secondary
+                session, primary=primary, secondary=secondary
             )
 
-        # Changed
         result["changed"] = modified_op or modified_op2
 
-    # Exit
+    if result["changed"]:
+        dns.apply()
+
     ansible_module.exit_json(**result)
 
 
